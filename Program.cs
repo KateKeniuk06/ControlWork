a9952141d2c6e086d006586a77ea709c5d493561
kate <dreaaaamtime@gmail.com>
2022-04-14 18:46:46 +0200

    second commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string[] array = { "456", "Hello", "-4", "879", "7", "world" };
+void pub (string[] array)
+
+{
+    int count = 0;
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string number = array[i];
+        if (number.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] arrayTwo = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        string number = array[i];
+        if (number.Length <= 3)
+        {
+            arrayTwo[j] = number;
+            Console.Write($"{arrayTwo[j]} ");
+            j++;
+        }
+
+    }
+
+}
+pub (array);
+
